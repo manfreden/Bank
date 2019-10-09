fc6fb6942c6b38aed58ae15884fd46738ab254f9
Emanuel Andersson <emanuel.andersson2@learnet.se>
2019-10-09 13:31:07 +0200

    Bank 1,5

--- bank.py
+++ bank.py
@@ -1,23 +1,31 @@
-pin = 1234
+pin = 1234 # variabeln tilldelas namnet pin och värdet 1234
 
-userPin = int(input("skriv in din pinkod: "))
+userPin = int(input("skriv in din pinkod: ")) # här frågar programmet om din pinkod
 
-if pin != userPin:
+if pin != userPin: # fel pinkod == error
     exit()
 
-saldo = 0.0
+f = open("saldo2.txt", "r") # variablen f tilldelas en funktion
+saldo = int(f.read())
+f.close()
+
 menu = 0
-# print ("menu 1 insättning")
-# print ("menu 2 uttag")
-# print ("menu 3 avsluta")
+
 while menu != 3:
     print("ditt saldo ät: " + str(saldo) + "Kr")
-    menu = int(input("skriv ditt val[1, 2, 3]: "))
-    if menu == 1:
+    menu = int(input("skriv ditt val[1, 2, 3]: ")) # konverterat en string til en int, stringen får funktionen att välja alternativ 1-3
+    if menu == 1:  # variabln 1 ges funktionen saldo + insättning
         print("insättning")
-        saldo = saldo + float(input("gör en insättning: "))
-    elif menu == 2:
+        saldo = saldo + int(input("gör en insättning: "))
+    elif menu == 2: # variabln 2 ges funktionen saldo - uttdrag
         print("uttag")
-        saldo = saldo - float(input("gör en uttag: "))
+        utdrag = int(input("gör en uttag: "))
+        saldo = saldo - utdrag
+        if saldo < 0:  
+            print("Du kan inte ta ut pengar du inte har")
+            saldo += utdrag # om saldo är mindre än noll nekas uttag
     else:
         print("fel eller avslut")
+f = open('saldo2.txt', 'w')
+f.write(str(saldo))
+f.close() #om 3 sparas saldot i textdokumentet och avslutar
